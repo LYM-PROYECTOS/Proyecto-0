a7a79aa20632473afd57bf4b30380d396b75c3d8
aarevalom0 <a.arevalom@uniandes.edu.co>
2023-08-23 08:19:52 -0500

    V1. sin bloques

--- Parser.py
+++ Parser.py
@@ -1,15 +1,40 @@
-
+#"D:\OneDrive - Universidad de los andes\U.ANDES\2023-2\LyM\4-PTO\Proyecto-0\Prueba.txt"
 file = open(input("Ingrese ruta de acceso del archivo con el programa (formato .txt): "))
 ReadFile = file.read()
-program = ReadFile.split("\n")
-Procesamiento = (0, True, program)
+comandos = ["JUMP","WALK","LEAP","TURN","TURNTO","DROP","GET","GRAB","LETGO","NOP", "FACING"]
+Procesamiento = (0, True, [],comandos)
+#FUNCIONES DE LECTURA ARCHIVO
+def tokenize_string(text):
+    Pro0=""
+    text= text.split("\n")
+    for item in text:
+        Pro0 += item.upper()
+        Pro0+=" "
+    
 
-def SaveProgram(Procesamiento):
-    Final_Program=[]
-    program=Procesamiento[2]
-    for i in range(len(program)):
-        palabra = program[i]
-        if palabra != " ":
+    PROGRAMA1 = []
+    palabra = ""
+    
+    for caracter in Pro0:
+        if caracter in "{}(),;=":
+            if palabra:
+                PROGRAMA1.append(palabra)
+                palabra = ""
+            PROGRAMA1.append(caracter)
+        elif caracter == " ":
+            if palabra:
+                PROGRAMA1.append(palabra)
+                palabra = ""
+        else:
+            palabra += caracter
+    
+    if palabra:
+        PROGRAMA1.append(palabra)
+
+     
+    return PROGRAMA1
 
 
-    return Procesamiento
+print(tokenize_string(ReadFile))
+
+#FUNCIONES DE VERIFICACION
